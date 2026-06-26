d585f030d2960763174619a481c876d7242e8b3f
holipay <holipay@gmail.com>
2026-06-27 07:46:38 +0800

    feat: add Linux TUN support and cross-platform client entry point
    - internal/tun/tun_linux.go: Linux TUN device using wireguard/tun
    - internal/tun/configure_linux.go: ip route/addr configuration
    - cmd/client/main_other.go: Linux client entry point
    - cmd/client/platform_other.go: stub for non-Windows platforms

---
+++ cmd/client/main_other.go
@@ -0,0 +1,55 @@
+//go:build !windows
+
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"runtime/debug"
+
+	"github.com/holipay/gametunnel/internal/client"
+	"github.com/holipay/gametunnel/internal/i18n"
+	"github.com/holipay/gametunnel/internal/tun"
+)
+
+func main() {
+	defer writeCrashLog()
+
+	cfg := client.LoadConfig()
+
+	if cfg.Lang != "" {
+		i18n.Set(i18n.ParseLang(cfg.Lang))
+	}
+
+	serverPublicIP := parseHostIP(cfg.ServerAddr)
+
+	tunFactory := func(tunCfg client.TunConfig) (client.TunDevice, error) {
+		return tun.New(tun.Config{
+			VirtualIP:      tunCfg.VirtualIP,
+			SubnetMask:     tunCfg.SubnetMask,
+			ServerIP:       tunCfg.ServerIP,
+			ServerPublicIP: serverPublicIP,
+			MTU:            tunCfg.MTU,
+		})
+	}
+
+	run(cfg, tunFactory)
+}
+
+func parseHostIP(addr string) net.IP {
+	host, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		return net.ParseIP(addr)
+	}
+	return net.ParseIP(host)
+}
+
+func writeCrashLog() {
+	r := recover()
+	if r == nil {
+		return
+	}
+	fmt.Fprintf(os.Stderr, "Panic: %v\n%s\n", r, debug.Stack())
+	os.Exit(1)
+}
