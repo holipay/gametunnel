4d650a40c7e27bc252d945ff598835e7fe8b59a6
holipay <holipay@gmail.com>
2026-06-27 11:08:31 +0800

    bump version to v1.5.0 (0x0105)

--- internal/protocol/protocol.go
+++ internal/protocol/protocol.go
@@ -20,8 +20,8 @@ const ProtocolVersion byte = 1
 
 // AppVersion is the application version encoded as (major << 8 | minor).
 // Used for client-server compatibility negotiation during handshake.
-// v1.3 = 0x0103 = 259
-const AppVersion uint16 = 0x0104
+// v1.3 = 0x0103 = 259, v1.4 = 0x0104 = 260
+const AppVersion uint16 = 0x0105
 
 // HeaderLen is the fixed header size: version(1) + type(1).
 const HeaderLen = 2
